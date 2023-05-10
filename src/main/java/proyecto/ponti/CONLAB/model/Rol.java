6041ed1e2242bc32c4e60149746892356e0da350
Ailto <70421252@elp.edu.pe>
2023-05-09 23:26:59 -0500

    commit de ailton cardenas

---
+++ src/main/java/proyecto/ponti/CONLAB/model/Rol.java
@@ -0,0 +1,18 @@
+package proyecto.ponti.CONLAB.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+@Entity
+public class Rol{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="idrol")
+    private Integer id;
+    private String usuario;
+    private String contraseña;
+
+}
